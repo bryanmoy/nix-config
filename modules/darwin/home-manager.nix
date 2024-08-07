fef3639e7fdd9b80972354729f3772208758ff09
Bryan Moy <dev@bryanmoy.com>
2024-08-08 01:06:01 +0200

    fix(1password): Identity Agent Path

--- modules/darwin/home-manager.nix
+++ modules/darwin/home-manager.nix
@@ -62,8 +62,8 @@ in
         ssh = {
           enable = true;
           extraConfig = ''
-            Host *
-                IdentityAgent "~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+          Host *
+            IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
           '';
         };
       } // import ../shared/home-manager.nix { inherit config pkgs lib; };
